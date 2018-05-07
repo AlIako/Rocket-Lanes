36c0e3f60bf879af11b74f4cdd7e67de5e4c4d7e
Alexis Iakovenko <alexis.iakovenko@gmail.com>
2018-05-07 16:00:27 +0200

    Create sample scene with movable character

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField]
+	float speed = 1.0f;
+	
+	new Rigidbody2D rigidbody;
+
+	void Start ()
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+	{
+		Move(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+	}
+
+	void Move(Vector2 direction)
+	{
+		const float factor = .1f;
+		transform.position = new Vector3(transform.position.x + direction.x * speed * factor, 
+										transform.position.y + direction.y * speed * factor, 
+										transform.position.z);
+	}
+}
