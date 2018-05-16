9552a5804d5d57e25dde3e7d463f6dfe5bb7a460
Alexis Iakovenko <alexis.iakovenko@gmail.com>
2018-05-16 13:04:58 +0200

    Customize player spawning

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager
+{
+	int nextPlayerId = 0;
+	NetworkStartPosition[] playerSpawns;
+
+	void Start()
+	{
+		FindPlayerSpawns();
+	}
+
+	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        GameObject player = (GameObject)Instantiate(playerPrefab, playerSpawns[nextPlayerId].transform.position, Quaternion.identity);
+        //player.GetComponent<Player>().color = Color.red;
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+
+		nextPlayerId ++;
+		if(nextPlayerId >= 4)
+			nextPlayerId = 0;
+    }
+
+	void FindPlayerSpawns()
+	{
+		playerSpawns = GameObject.FindObjectsOfType<NetworkStartPosition>();
+	}
+}
