67b44fea83feded70c1d2e3fb8236bebdec0aca2
Alexis Iakovenko <alexis.iakovenko@gmail.com>
2018-05-16 14:47:35 +0200

    Create interface to chose between network models

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+	Player player;
+	
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/NetworkChoser.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkChoser : MonoBehaviour
+{
+	[SerializeField]
+	GameObject inGameUI;
+	
+	[SerializeField]
+	GameObject lobbyUI;
+	
+	[SerializeField]
+	GameObject serverClientNetworkManager;
+
+	public enum NetworkType {Singleplayer, ServerClient, P2P};
+	[SerializeField]
+	private NetworkType networkType = NetworkType.Singleplayer;
+	public NetworkType GetNetworkType { get { return networkType; } }
+
+	public void PickNetworkType(int networkType)
+	{
+		//can't pass enum as parameter of OnClick callback
+		if(networkType == 0)
+			this.networkType = NetworkType.Singleplayer;
+		else if(networkType == 1)
+			this.networkType = NetworkType.ServerClient;
+		else if(networkType == 2)
+			this.networkType = NetworkType.P2P;
+
+		lobbyUI.SetActive(false);
+		inGameUI.SetActive(true);
+
+		if(this.networkType == NetworkType.ServerClient)
+			serverClientNetworkManager.SetActive(true);
+	}
+}
