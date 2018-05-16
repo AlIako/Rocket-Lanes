f53d9e29cd4b2572b84708ac8df5317cb3f2a155
Alexis Iakovenko <alexis.iakovenko@gmail.com>
2018-05-16 12:47:49 +0200

    Add send rocket button

---
+++ Assets/Scripts/SpawnerManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{
+	[SerializeField]
+	GameObject projectile;
+
+	[SerializeField]
+	float timeBetweenSpawn = 1.0f;
+
+	[SerializeField]
+	bool alwaysSpawning = false;
+
+	List<GameObject> spawners;
+	float lastSpawn = 0.0f;
+
+	void Start ()
+	{
+		FindSpawners();
+		lastSpawn = Time.time;
+	}
+	
+	void Update ()
+	{
+		AlwaysSpawn();
+	}
+
+	public GameObject GetFromPool()
+	{
+		int index = Random.Range(0, spawners.Count);
+		return spawners[index];
+	}
+
+	void AlwaysSpawn()
+	{
+		if(alwaysSpawning)
+		{
+			if(Time.time - lastSpawn >= timeBetweenSpawn)
+			{
+				Spawn();
+				lastSpawn = Time.time;
+			}
+		}
+	}
+
+	void FindSpawners()
+	{
+		spawners = new List<GameObject>();
+		Transform parent = transform.parent;
+		foreach (Transform child in parent)
+		{
+			if(child.gameObject.GetComponent<Spawner>() != null)
+				spawners.Add(child.gameObject);
+		}
+	}
+
+	public GameObject Spawn()
+	{
+		int index = Random.Range(0, spawners.Count);
+		GameObject spawner = spawners[index];
+		return Instantiate(projectile, spawner.transform.position, spawner.transform.rotation);
+	}
+}
---
+++ Assets/Scripts/UIGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class UIGame : MonoBehaviour
+{
+	[SerializeField]
+	SpawnerManager spawnerManager;
+
+	NetworkManager networkManager;
+
+	void Start()
+	{
+		networkManager = GameObject.FindObjectOfType<NetworkManager>();
+	}
+	
+	public void SendRocket()
+	{
+		NetworkServer.Spawn(spawnerManager.Spawn());
+	}
+}
