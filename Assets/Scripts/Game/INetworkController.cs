88f14ba5a9b7704ff6e0a42ee4b3e6dd7aee9034
Alexis Iakovenko <alexis.iakovenko@gmail.com>
2018-08-01 14:13:12 +0200

    Add CastShield ConsentAction

--- Assets/Scripts/Game/INetworkController.cs
+++ Assets/Scripts/Game/INetworkController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 
-public enum ConsentAction {SpawnRocket};
+public enum ConsentAction {SpawnRocket, CastShield};
 
 public interface INetworkController
 {
